94b67f74ad3722947d2ba4fb900026d19e057dcb
Your Name <you@example.com>
2026-06-10 18:30:15 +0900

    add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 <div class="video-background-container">
 	<div class="video-overlay"></div>
 	<iframe
-		src="https://www.youtube.com/embed/BpXlJnRw3OY?autoplay=1&mute=1&loop=1&playlist=BpXlJnRw3OY&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&playsinline=1&enablejsapi=1"
+		src="https://www.youtube.com/embed/BpXlJnRw3OY?autoplay=1&mute=1&loop=1&playlist=BpXlJnRw3OY,z8XyD-4lU_c&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&playsinline=1&enablejsapi=1"
 		title="Dolphins Swimming Background"
 		frameborder="0"
 		allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
